c2eb4c83490ca0a2aec5d9d047a4259d6d66823e
Arpit Raj <arpitdubey907@gmail.com>
2021-05-15 00:45:25 +0530

    Add files via upload

---
+++ Binary_search_tree/Find_minumum_element_inBST.cpp
@@ -0,0 +1,12 @@
+//Function to find the minimum element in the given BST.
+int minValue(Node* root)
+{
+    Node* current = root; 
+  
+/* loop down to find the leftmost leaf */
+while (current->left != NULL) 
+{ 
+    current = current->left; 
+} 
+return(current->data); 
+}
