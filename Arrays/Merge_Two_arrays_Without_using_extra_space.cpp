ef28c2d72c1c25927bd2c6dc3e0681ce2e4ef396
Arpit Raj <arpitdubey907@gmail.com>
2021-07-01 23:10:03 +0530

    Update Merge_Two_arrays_Without_using_extra_space.cpp

--- Arrays/Merge_Two_arrays_Without_using_extra_space.cpp
+++ Arrays/Merge_Two_arrays_Without_using_extra_space.cpp
@@ -1,74 +1,66 @@
 // { Driver Code Starts
-#include<bits/stdc++.h>
-
-using namespace std;
+#include <bits/stdc++.h> 
+using namespace std; 
 
 
  // } Driver Code Ends
-
 class Solution{
-
-    // Function to find the trapped water between the blocks.
     public:
-
-    int trappingWater(int arr[], int n){
-         // left[i] contains height of tallest bar to the
-    // left of i'th bar including itself
-    int left[n];
- 
-    // Right [i] contains height of tallest bar to
-    // the right of ith bar including itself
-    int right[n];
- 
-    // Initialize result
-    int water = 0;
- 
-    // Fill left array
-    left[0] = arr[0];
-    for (int i = 1; i < n; i++)
-        left[i] = max(left[i - 1], arr[i]);
- 
-    // Fill right array
-    right[n - 1] = arr[n - 1];
-    for (int i = n - 2; i >= 0; i--)
-        right[i] = max(right[i + 1], arr[i]);
- 
-    // Calculate the accumulated water element by element
-    // consider the amount of water on i'th bar, the
-    // amount of water accumulated on this particular
-    // bar will be equal to min(left[i], right[i]) - arr[i] .
-    for (int i = 0; i < n; i++)
-        water += min(left[i], right[i]) - arr[i];
- 
-    return water;
-    }
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            for(int i=0;i<n;i++)
+            {
+                if(arr1[i]>arr2[0])
+                {
+                    swap(arr1[i],arr2[0]);
+                    int k;
+                    int secondarr=arr2[0];
+                    for(k=1;k<m && arr2[k]<secondarr;k++)
+                    {
+                        arr2[k-1]=arr2[k];    
+                    }
+                    arr2[k-1]=secondarr;
+                }
+            }
+            // code here 
+        } 
 };
 
 // { Driver Code Starts.
 
-int main(){
-    
-    int t;
-    //testcases
-    cin >> t;
-    
-    while(t--){
-        int n;
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
         
-        //size of array
-        cin >> n;
-        
-        int a[n];
-        
-        //adding elements to the array
-        for(int i =0;i<n;i++){
-            cin >> a[i];            
-        }
-        Solution obj;
-        //calling trappingWater() function
-        cout << obj.trappingWater(a, n) << endl;
-        
-    }
-    
-    return 0;
-}  // } Driver Code Ends
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
