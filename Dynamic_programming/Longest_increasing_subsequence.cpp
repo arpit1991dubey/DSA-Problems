495a40470d7e92f33436454e21d0195a0587cf29
Arpit Raj <arpitdubey907@gmail.com>
2021-07-05 19:56:15 +0530

    Create Longest_increasing_subsequence.cpp

---
+++ Dynamic_programming/Longest_increasing_subsequence.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+
+class Solution
+{
+    public:
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int arr[])
+    {
+    int lis[n];
+ 
+    lis[0] = 1;
+ 
+    /* Compute optimized LIS values in
+       bottom up manner */
+    for (int i = 1; i < n; i++) {
+        lis[i] = 1;
+        for (int j = 0; j < i; j++)
+            if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                lis[i] = lis[j] + 1;
+    }
+ 
+    // Return maximum value in lis[]
+    return *max_element(lis, lis + n);
+}
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        //taking size of array
+        cin>>n;
+        int a[n];
+        
+        //inserting elements to the array
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        //calling method longestSubsequence()
+        cout << ob.longestSubsequence(n, a) << endl;
+    }
+}
+  // } Driver Code Ends
