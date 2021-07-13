1b234af053ce64f8a7101ff1842ddd36ce3bcdbc
Arpit Raj <arpitdubey907@gmail.com>
2021-07-13 23:21:00 +0530

    Create Sum Root to Leaf Numbers.cpp

---
+++ Trees/Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,53 @@
+/*
+129. Sum Root to Leaf Numbers
+Medium
+
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+    For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+ 
+
+Example 1:
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    
+    void preorder(TreeNode* node){
+        if(!node) return;
+      
+        preorder(node->left);
+        ans.push_back(node->val);
+        preorder(node->right);
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        preorder(root);
+        return ans;
+    }
+};
