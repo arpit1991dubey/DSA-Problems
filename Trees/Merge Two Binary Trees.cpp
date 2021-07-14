287893cb4acf342e69eb8faeefbcfb2bf75fd69d
Arpit Raj <arpitdubey907@gmail.com>
2021-07-14 13:24:42 +0530

    Create Merge Two Binary Trees.cpp

---
+++ Trees/Merge Two Binary Trees.cpp
@@ -0,0 +1,49 @@
+/*617. Merge Two Binary Trees
+Easy
+
+You are given two binary trees root1 and root2.
+
+Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+Return the merged tree.
+
+Note: The merging process must start from the root nodes of both trees.
+
+ 
+
+Example 1:
+
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
+
+Example 2:
+
+Input: root1 = [1], root2 = [1,2]
+Output: [2,2]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* A, TreeNode* B) {
+       if (!A) return B;
+      if (!B) return A;
+    
+    A->val += B->val;
+    A->left = mergeTrees(A->left,B->left);
+    A->right = mergeTrees(A->right,B->right);
+    return A;
+        
+    
+    }
+};
