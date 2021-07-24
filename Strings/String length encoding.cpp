b148c8ab51138258bd3d104fbe288642bf2a1eb7
Arpit Raj <arpitdubey907@gmail.com>
2021-07-24 22:45:36 +0530

    Update String length encoding.cpp

--- Strings/String length encoding.cpp
+++ Strings/String length encoding.cpp
@@ -3,31 +3,48 @@
 String length encoding - convert a string from- aaaaabbbsss to a5b3s3
 
 *******************************************************************************/
-#include <stdio.h>
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-string compressString(string str){   
+
+bool uniqueCharacters(string str)
+{
+ 
+    // Using sorting
+    sort(str.begin(), str.end());
+ 
+    for (int i = 0; i < str.length()-1; i++) {
+ 
+        // if at any time, 2 adjacent
+        // elements become equal,
+        // return false
+        if (str[i] == str[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+//str is the input the string
+string compressString(const string &str){   
     //complete the function to return output string
-    
+    string stri=str;
+    int count=0;
     map<char,int>store;
-    for(int i=0;i<str.length();i++)
+    for(int i=0;i<stri.length();i++)
     {
-        store[str[i]]++;
+        store[stri[i]]++;
     }
     string res="";
     for(auto x:store)
     {
-        res=res+x.first+to_string(x.second);
-
+        if(uniqueCharacters(stri))
+        {
+                res=stri;
+        }
+            else
+            {
+                        res=res+x.first+to_string(x.second);
+            }
     }
-    
    return res;
-}
-int main()
-{
-    string big,small;
-    cout<<"Enter the bigger string";
-    getline(cin,big);
-    cout<<compressString(big);
-    return 0;
 }
