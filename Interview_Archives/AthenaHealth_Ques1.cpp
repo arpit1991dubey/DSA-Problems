e35a8ec615570a590bd1dca8769464df819da11a
Arpit Raj <arpitdubey907@gmail.com>
2021-08-19 14:16:02 +0530

    Update AthenaHealth_Ques1.cpp

--- Interview_Archives/AthenaHealth_Ques1.cpp
+++ Interview_Archives/AthenaHealth_Ques1.cpp
@@ -44,3 +44,53 @@ int main()
 cout<<count;
     return 0;
 }
+
+
+
+
+
+
+/*
+in the same question if asked to move odd numbers to the start and even to the end.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int i=0,j=n-1;
+    while(i<=j)
+    {
+        if(arr[i]%2!=0 && arr[i]%2==0)
+        {
+            i++;
+            j--;
+        }
+        else if(arr[i]%2==0 && arr[j]%2!=0)
+        {
+            swap(arr[j],arr[i]);
+            i++,j--;
+            count++;
+        }
+        else if(arr[i]%2==0 && arr[j]%2==0 )
+        {
+            j--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+cout<<count;
+    return 0;
+}
+
