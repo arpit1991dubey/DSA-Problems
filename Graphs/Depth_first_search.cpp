61c4bafb124b6afb9cf0fc59aefdc22c95b45b57
Arpit Raj <arpitdubey907@gmail.com>
2021-08-21 15:17:06 +0530

    Create Depth_first_search.cpp

---
+++ Graphs/Depth_first_search.cpp
@@ -0,0 +1,18 @@
+ void dfs(int node, vector<int> &vis, vector<int> adj[], vector<int> &storeDfs) {
+        storeDfs.push_back(node); 
+        vis[node] = 1; 
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                dfs(it, vis, adj, storeDfs); 
+            }
+        }
+    }
+public:
+	vector<int>dfsOfGraph(int V, vector<int> adj[]){
+	    vector<int> storeDfs; 
+	    vector<int> vis(V+1, 0); 
+      for(int i = 1;i<=V;i++) {
+        if(!vis[i]) dfs(i, vis, adj, storeDfs); 
+      }
+	    return storeDfs; 
+	}
