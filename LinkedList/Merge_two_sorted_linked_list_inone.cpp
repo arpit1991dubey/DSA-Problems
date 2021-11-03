0fcf896671f544ebfcc75221e74e1a31b8d1a429
Arpit Raj <arpitdubey907@gmail.com>
2021-11-03 21:39:15 +0530

    Update Merge_two_sorted_linked_list_inone.cpp

--- LinkedList/Merge_two_sorted_linked_list_inone.cpp
+++ LinkedList/Merge_two_sorted_linked_list_inone.cpp
@@ -3,29 +3,39 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1==NULL) return l2;
-        if(l2==NULL) return l1;
-        if(l1->val > l2->val) swap(l1,l2);
-        ListNode *res=l1;
-        while(l1!=NULL and l2!=NULL)
-        {
-            ListNode *tmp=NULL;
-            while(l1!=NULL && l1->val <= l2->val)
-            {
-                tmp=l1;
-                l1=l1->next;
+ListNode* Solution::mergeTwoLists(ListNode* l1, ListNode* l2) {
+            if(l1 == NULL) return l2;
+            if(l2 == NULL) return l1;
+            ListNode* head = NULL;    // head of the list to return
+            // find first element (can use dummy node to put this part inside of the loop)
+            if(l1->val < l2->val) { 
+                head = l1; 
+                l1 = l1->next; 
+            } else { 
+                head = l2; 
+                l2 = l2->next; 
             }
-            tmp->next=l2;
-            swap(l1,l2);
-        }
-        return res;
-    }
-};
+            ListNode* p = head;     // pointer to form new list
+            while(l1 && l2){
+                if(l1->val < l2->val) { 
+                    p->next = l1; 
+                    l1 = l1->next; 
+                } else { 
+                    p->next = l2; 
+                    l2 = l2->next; 
+                }
+                p = p->next;
+            }
+            // add the rest of the tail, done!
+            if (l1) { 
+                p->next=l1;
+            } else {
+                p->next=l2;
+            }
+
+            return head;
+}
+
